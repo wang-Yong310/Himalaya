9b4df61d48c6ddfdff7511918dc9a159575bdd3d
wangyong12 <wangyong12@xiaomi.com>
2020-11-30 15:44:47 +0800

    集成喜马拉雅sdk并且测试

---
+++ HimalayaProject/app/src/main/java/com/example/himalayaproject/base/BaseApplication.java
@@ -0,0 +1,20 @@
+package com.example.himalayaproject.base;
+
+import android.app.Application;
+
+import com.ximalaya.ting.android.opensdk.constants.DTransferConstants;
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (DTransferConstants.isRelease) {
+            CommonRequest mXimalaya = CommonRequest.getInstanse();
+            String appSecret = "19c8428803e104ac93d7cf154521ae81";
+            mXimalaya.setAppkey("920afe193099eed8e4321678157eea8e");
+            mXimalaya.setPackid("com.example.himalayaproject");
+            mXimalaya.init(this, appSecret);
+        }
+    }
+}
